4194e8bbeccaa5cf6547e657461bfa261cbb8abe
avemike <fajny.michal.gajek@gmail.com>
2020-09-02 13:49:48 +0200

    moved unhandledHandler to logger file

--- src/logger.ts
+++ src/logger.ts
@@ -2,6 +2,17 @@ import pino from 'pino';
 import { hostname } from 'os';
 import { version } from '../package.json';
 
+const unhandledHandler = (message: string) =>
+  pino.final(logger, (error: Error) => {
+    logger.error(error, message);
+
+    process.exit(1);
+  });
+
+process
+  .on('unhandledRejection', unhandledHandler('unhandledRejection'))
+  .on('uncaughtException', unhandledHandler('uncaughtException'));
+
 const level = (() => {
   switch (process.env.NODE_ENV) {
     case 'test':
